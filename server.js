7056dc000dfaa418d0003b9c8a437bf77856f89b
gitkcb <kcharlesbell@gmail.com>
2022-12-07 17:54:28 -0500

    added inquirer prompt

---
+++ server.js
@@ -0,0 +1,26 @@
+import inquirer from 'inquirer';
+import mysql  from 'mysql2';
+import consoleTable from 'console.table';
+
+const PORT = process.env.PORT || 3001;
+
+
+
+const db = mysql.createConnection({    
+    host: 'localhost',
+    user: 'root',
+    password: "Winesciencevision420$",
+    database: "employee_db",
+},
+console.log('Connected to the employees_db database')
+);
+
+inquirer.prompt([
+    {
+        type: "list",
+        name: "openingchoice",
+        message: "What would you like to do?",
+        choices: ["View All Employees", "Add Employer", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "View All Employees", "Quit"],
+
+    }
+])
